72190534204f0b98ac6e243a606b6f3a8b371b76
Arsalehi <arsalehi@mbank.local>
2026-07-29 23:28:32 +0330

    fix: correct Persian day-of-week mapping in PersianDate component

--- client/src/components/persian/PersianDate.jsx
+++ client/src/components/persian/PersianDate.jsx
@@ -4,7 +4,7 @@ import { toPersian, PERSIAN_DAYS, PERSIAN_MONTHS } from './persianDate.js'
 const PersianDate = () => {
   const now = new Date()
   const p = toPersian(now)
-  const dayName = PERSIAN_DAYS[(now.getDay() + 6) % 7]
+  const dayName = PERSIAN_DAYS[(now.getDay() + 1) % 7]
   const monthName = PERSIAN_MONTHS[p.month - 1]
 
   return (
